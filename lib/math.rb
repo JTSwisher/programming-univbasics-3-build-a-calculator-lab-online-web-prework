bd2cfcc897253a29d7aebfb297ee1ac88d8d9297
Jeff Swisher <jefftswisher@gmail.com>
2019-09-22 04:51:24 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,29 @@
 def addition(num1, num2)
-
+  num1 = 5
+  num2 = 4
+  puts (num1  + num2)
+  return num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  num1 = 10
+  num2 = 5
+  puts (num1 - num2)
+  return num1 - num2
 end
 
 def division(num1, num2)
-
+  num1 = 50
+  num1 = 2
+  puts (num1 / num2)
+  return num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  num1 = 4
+  num2 = 30
+  puts (num1 * num2)
+  return num1 * num2
 end
 
 def modulo(num1, num2)
